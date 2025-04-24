7e75b3446fe9d8f6a035d07ce46a432aaeb35693
Arshia <aliarshiamohebbi@gmail.com>
2025-04-24 16:23:00 +0330

    Enum animal update

--- src/main/java/models/animals/AnimalProductType.java
+++ src/main/java/models/animals/AnimalProductType.java
@@ -1,8 +1,19 @@
 package models.animals;
 
 public enum AnimalProductType {
-    EGG(50);
-
+    EGG(50),
+    LARGE_EGG(95),
+    DUCK_EGG(95),
+    DUCK_FEATHER(250),
+    RABBIT_FLUFF(340),
+    RABBIT_FOOT(565),
+    DINOSAUR_EGG(1400),
+    COW_MILK(125),
+    COW_LARGE_MILK(190),
+    GOAT_MILK(225),
+    GOAT_LARGE_MILK(345),
+    SHEEP_FLUFF(340),
+    TRUFFLE(625);
     public final int price;
 
     AnimalProductType(int price) {
