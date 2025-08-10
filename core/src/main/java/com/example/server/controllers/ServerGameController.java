2bb2e1c41d16a44850b61d1924fb58570ecd5e44
arvinasli <asliarvin@gmail.com>
2025-08-10 17:49:40 +0330

    multiplayer attempt (1)

--- core/src/main/java/com/example/server/controllers/ServerGameController.java
+++ core/src/main/java/com/example/server/controllers/ServerGameController.java
@@ -20,7 +20,6 @@ public class ServerGameController {
         //    case "set_randomizers", "player_movement", "player_stop", "flower", "marry-request", "talk",
         //         "generate_trees", "generate_stones", "predict_weather","hug", "gift" , "rateGift" , "marry-response" , "send_emoji"-> {
         reflectMessage(req, respBody);
-            //}
     }
 
     public static void reflectMessage(Message req, Map<String,Object> respBody) {
